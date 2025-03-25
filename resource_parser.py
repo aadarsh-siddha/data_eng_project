fc713b49f1da1953f1fc25f518800f8d4099b1ab
Aadarsh Siddha <siddha.a@northeastern.edu>
2025-03-25 18:55:17 -0400

    added a few parsers

--- resource_parser.py
+++ resource_parser.py
@@ -1,4 +1,6 @@
 def codeable_concept_parser(cc):
+    if cc == None:
+        return None
     code = ""
 
     for i,codings in enumerate(cc.coding):
@@ -8,12 +10,26 @@ def codeable_concept_parser(cc):
     return code
 
 def n_codeable_concept_parser(codeable_list):
+    if codeable_list == None:
+        return None
     for i,codeable in enumerate(codeable_list):
         codes = codeable_concept_parser(codeable)
         if i > 0 and i == len(codeable_list) - 1:
             codes += ","
 
     return codes
+
+def care_team_participant_parser(participants):
+    rows = []
+    for participant in participants:
+        participant_schema = dict()
+        participant_schema['ID'] = participant.member.reference
+        participant_schema['name'] = participant.member.display
+        participant_schema['role_code'] = n_codeable_concept_parser(participant.role)
+
+        rows.append(participant_schema)
+
+    return rows
 
 def patient_resource_parser(pat):
     patient_schema = dict()
@@ -255,3 +271,177 @@ def observation_resource_parser(obs):
     observation_schema['component'] = getattr(obs, 'component', None)
 
     return observation_schema    
+
+
+def care_team_resource_parser(care_team):
+    if care_team.id == None:
+        return None
+    care_team_schema = dict()
+    care_team_schema['id'] = care_team.id
+    care_team_schema['status'] = getattr(care_team,'status',None)
+    care_team_schema['category_code'] = n_codeable_concept_parser(care_team.category)
+    care_team_schema['name'] = getattr(care_team,'name',None)
+    care_team_schema['patient_id'] = getattr(getattr(care_team,'subject',None),'reference',None)
+    care_team_schema['encounter_id'] = getattr(getattr(care_team,'encounter',None),'reference',None)
+    care_team_schema['period_start'] = getattr(getattr(care_team,'period',None),'start',None)
+    care_team_schema['period_end'] = getattr(getattr(care_team,'period',None),'end',None)
+    care_team_schema['participants'] = care_team_participant_parser(care_team.participant)
+    care_team_schema['reasonCode'] = n_codeable_concept_parser(getattr(care_team,'reasonCode',None))
+    care_team_schema['reasonReference'] = getattr(care_team,'reasonReference',None)
+    care_team_schema['note'] = getattr(care_team,'note',None)
+    care_team_schema['telecom'] = getattr(care_team,'telecom',None)
+    for i,org in enumerate(care_team.managingOrganization):
+        care_team_schema[f'managing_org_{i+1}_name'] = org.display     
+        care_team_schema[f'managing_org_{i+1}_id'] = org.reference
+
+    return care_team_schema
+
+def care_plan_activity_parser(activities):
+    rows = []
+    for activity in activities:
+        activity_schema = dict()
+        activity_schema['outcome_code'] = n_codeable_concept_parser(activity.outcomeCodeableConcept)
+        activity_schema['outcomeReference'] = getattr(activity,'outcomeReference',None)
+        activity_schema['progress'] = getattr(activity,'progress',None)
+        activity_schema['reference'] = getattr(activity,'reference',None)
+        activity_schema['note'] = getattr(activity,'note',None)
+        activity_schema['instantiates_canonical'] = getattr(getattr(activity,'detail',None),'instantiatesCanonical',None)
+        activity_schema['instantiates_uri'] = getattr(getattr(activity,'detail',None),'instantiatesUri',None)
+        activity_schema['code'] = codeable_concept_parser(getattr(getattr(activity,'detail',None),'code',None))
+        activity_schema['reason_code'] = n_codeable_concept_parser(getattr(getattr(activity,'detail',None),'reasonCode',None))
+        activity_schema['reason_reference'] = getattr(getattr(activity,'detail',None),'reasonReference',None)
+        activity_schema['goal'] = getattr(getattr(activity,'detail',None),'goal',None)
+        activity_schema['status'] = getattr(getattr(activity,'detail',None),'status',None)
+        activity_schema['status_reason'] = getattr(getattr(activity,'detail',None),'statusReason',None)
+        activity_schema['do_not_perform'] = getattr(getattr(activity,'detail',None),'doNotPerform',None)
+        activity_schema['location'] = getattr(getattr(activity,'detail',None),'location',None).display
+        activity_schema['daily_amount'] = getattr(getattr(activity,'detail',None),'dailyAmount',None)
+        activity_schema['product'] = getattr(getattr(activity,'detail',None),'product',None)
+        rows.append(activity_schema)
+
+    return rows
+
+
+def care_plan_resource_parser(care_plan):
+    care_plan_schema = dict()
+    care_plan_schema['id'] = getattr(care_plan,'id',None)
+    care_plan_schema['instantiates_canonical'] = getattr(care_plan,'instantiatesCanonical',None)
+    care_plan_schema['instantiatesUri'] = getattr(care_plan,'instantiatesUri',None)
+    care_plan_schema['basedOn'] = getattr(care_plan,'basedOn',None)
+    care_plan_schema['partOf'] = getattr(care_plan,'partOf',None)
+    care_plan_schema['status'] = getattr(care_plan,'status',None)
+    care_plan_schema['intent'] = getattr(care_plan,'intent',None)
+    care_plan_schema['category_code'] = n_codeable_concept_parser(getattr(care_plan,'category',None))
+    care_plan_schema['title'] = getattr(care_plan,'title',None)
+    care_plan_schema['subject'] = getattr(getattr(care_plan,'subject',None),'reference',None)
+    care_plan_schema['encounter'] = getattr(getattr(care_plan,'encounter',None),'reference',None)
+    care_plan_schema['period_start'] = getattr(getattr(care_plan,'period',None),'start',None)
+    care_plan_schema['period_end'] = getattr(getattr(care_plan,'period',None),'end',None)
+    care_plan_schema['created'] = getattr(care_plan,'created',None)
+    care_plan_schema['author'] = getattr(care_plan,'author',None)
+    care_plan_schema['contributor'] = getattr(care_plan,'contributor',None)
+    care_plan_schema['created'] = getattr(care_plan,'created',None)
+    for i,care_team in enumerate(getattr(care_plan,'careTeam',None)):
+        care_plan_schema[f'care_team_{i+1}_id'] = care_team.reference
+    care_plan_schema['addresses'] = getattr(care_plan,'addresses',None)
+    care_plan_schema['created'] = getattr(care_plan,'created',None)
+    care_plan_schema['goal'] = getattr(care_plan,'goal',None)
+    care_plan_schema['activities'] = care_plan_activity_parser(care_plan.activity)
+
+    return care_plan_schema
+
+
+def allergy_intolerance_resource_parser(allergy_intolerance):
+    allergy_intolerance_schema = dict()
+    allergy_intolerance_schema['id'] = getattr(allergy_intolerance,'id',None)
+    allergy_intolerance_schema['clinical_status'] = codeable_concept_parser(getattr(allergy_intolerance,'clinicalStatus',None))
+    allergy_intolerance_schema['verification_status'] = codeable_concept_parser(getattr(allergy_intolerance,'verificationStatus',None))
+    allergy_intolerance_schema['type'] = getattr(allergy_intolerance,'type',None)
+    allergy_intolerance_schema['category'] = getattr(allergy_intolerance,'category',None)
+    allergy_intolerance_schema['criticality'] = getattr(allergy_intolerance,'criticality',None)
+    allergy_intolerance_schema['patient_id'] = getattr(getattr(allergy_intolerance,'patient',None),'reference',None)
+    allergy_intolerance_schema['encounter_id'] = getattr(getattr(allergy_intolerance,'encounter',None),'reference',None)
+    allergy_intolerance_schema['onset_age'] = getattr(allergy_intolerance,'onsetAge',None)
+    allergy_intolerance_schema['recorded_date'] = getattr(allergy_intolerance,'recordedDate',None)
+    allergy_intolerance_schema['onset_period'] = getattr(allergy_intolerance,'onsetPeriod',None)
+    allergy_intolerance_schema['onset_range'] = getattr(allergy_intolerance,'onsetRange',None)    
+    allergy_intolerance_schema['recorder'] = getattr(allergy_intolerance,'recorder',None)
+    allergy_intolerance_schema['asserter'] = getattr(allergy_intolerance,'asserter',None)
+    allergy_intolerance_schema['note'] = getattr(allergy_intolerance,'note',None)
+    allergy_intolerance_schema['last_occurrence'] = getattr(allergy_intolerance,'lastOccurrence',None)
+    allergy_intolerance_schema['reaction_substance'] = codeable_concept_parser(getattr(getattr(allergy_intolerance,'reaction',None),'substance',None))
+    allergy_intolerance_schema['reaction_manifestation'] = n_codeable_concept_parser(getattr(getattr(allergy_intolerance,'reaction',None),'manifestation',None))
+    allergy_intolerance_schema['reaction_onset'] = getattr(getattr(allergy_intolerance,'reaction',None),'onset',None)
+    allergy_intolerance_schema['reaction_severity'] = getattr(getattr(allergy_intolerance,'reaction',None),'severity',None)
+    allergy_intolerance_schema['reaction_exposure_route'] = codeable_concept_parser(getattr(getattr(allergy_intolerance,'reaction',None),'exposureRoute',None))
+
+    return allergy_intolerance_schema
+
+
+def dosage_instruction_parser(dosage_instructions):
+    return None
+
+def medication_request_resource_parser(medication_request):
+    medication_request_schema = dict()
+    medication_request_schema['id'] = getattr(medication_request,'id',None)
+    medication_request_schema['status'] = getattr(medication_request,'status',None)
+    medication_request_schema['status_reason'] = codeable_concept_parser(getattr(medication_request,'statusReason',None))
+    medication_request_schema['intent'] = getattr(medication_request,'intent',None)
+    medication_request_schema['category'] = n_codeable_concept_parser(getattr(medication_request,'category',None))
+    medication_request_schema['priority'] = getattr(medication_request,'priority',None)
+    medication_request_schema['do_not_perform'] = getattr(medication_request,'doNotPerform',None)
+    medication_request_schema['reported_boolean'] = getattr(medication_request,'reportedBoolean',None)
+    medication_request_schema['reported_reference_id'] = getattr(getattr(medication_request,'reportedReference',None),'reference',None)
+    medication_request_schema['subject'] = getattr(getattr(medication_request,'subject',None),'reference',None)
+    medication_request_schema['encounter'] = getattr(getattr(medication_request,'encounter',None),'reference',None)
+    medication_request_schema['authored_on'] = getattr(medication_request,'authoredOn',None)
+    medication_request_schema['requester'] = getattr(getattr(medication_request,'requester',None),'reference',None)
+    medication_request_schema['performer'] = getattr(medication_request,'performer',None)
+    medication_request_schema['performer_type'] = getattr(medication_request,'performerType',None)
+    medication_request_schema['recorder'] = getattr(medication_request,'recorder',None)
+    medication_request_schema['reason_code'] = n_codeable_concept_parser(getattr(medication_request,'reasonCode',None))
+    medication_request_schema['instantiates_canonical'] = getattr(medication_request,'instantiatesCanonical',None)
+    medication_request_schema['based_on'] = getattr(medication_request,'basedOn',None)
+    medication_request_schema['group_identifier'] = getattr(medication_request,'groupIdentifier',None)
+    medication_request_schema['course_of_therapy_type'] = getattr(medication_request,'courseOfTherapyType',None)
+    medication_request_schema['insurance'] = getattr(medication_request,'insurance',None)
+    medication_request_schema['note'] = getattr(medication_request,'note',None)
+    medication_request_schema['dosage_instruction'] = dosage_instruction_parser(getattr(medication_request,'dosageInstruction',None))
+    medication_request_schema['dispense_request'] = getattr(medication_request,'dispenseRequest',None)
+    medication_request_schema['dispense_request'] = getattr(medication_request,'dispenseRequest',None)
+
+    return medication_request_schema
+    
+
+def procedure_resource_parser(procedure):
+    procedure_schema = dict()
+    procedure_schema['id'] = getattr(procedure,'id',None)
+    procedure_schema['instantiates_canonical'] = getattr(procedure,'instantiatesCanonical',None)
+    procedure_schema['based_on'] = getattr(procedure,'basedOn',None)
+    procedure_schema['part_of'] = getattr(procedure,'partOf',None)
+    procedure_schema['status'] = getattr(procedure,'status',None)
+    procedure_schema['status_reason'] = codeable_concept_parser(getattr(procedure,'statusReason',None))
+    procedure_schema['code'] = codeable_concept_parser(getattr(procedure,'code',None))
+    procedure_schema['category'] = codeable_concept_parser(getattr(procedure,'category',None))
+    procedure_schema['subject'] = getattr(getattr(procedure,'subject',None),'reference',None)
+    procedure_schema['encounter'] = getattr(getattr(procedure,'encounter',None),'reference',None)
+    procedure_schema['performed_age'] = getattr(procedure,'performedAge',None)
+    procedure_schema['performed_datetime'] = getattr(procedure,'performedDateTime',None)
+    procedure_schema['performed_period_start'] = getattr(getattr(procedure,'performedPeriod',None),'start',None)
+    procedure_schema['performed_period_end'] = getattr(getattr(procedure,'performedPeriod',None),'end',None)
+    procedure_schema['performed_string'] = getattr(procedure,'performedString',None)
+    procedure_schema['recorder'] = getattr(procedure,'recorder',None)
+    procedure_schema['asserter'] = getattr(procedure,'asserter',None)
+    procedure_schema['performer'] = getattr(procedure,'performer',None)
+    procedure_schema['location'] = getattr(getattr(procedure,'location',None),'reference',None)
+    procedure_schema['reason_code'] = getattr(procedure,'reasonCode',None)
+    procedure_schema['body_site'] = getattr(procedure,'bodySite',None)
+    procedure_schema['outcome'] = getattr(procedure,'outcome',None)
+    if getattr(procedure,'reasonReference',None):
+        procedure_schema['reason_reference'] = procedure.reasonReference[0].reference
+    else:
+        procedure_schema['reason_reference'] = None
+    return procedure_schema
+
+
+
